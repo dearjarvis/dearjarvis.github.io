669971fe12f187ca92a142b75d3d2e7d126724c0
rbse <77785147+rbse@users.noreply.github.com>
2021-01-21 23:47:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -199,11 +199,11 @@ function startTimer(time){
 }
 
 function startTimerLine(time){
-    counterLine = setInterval(timer, 29);
+    counterLine = setInterval(timer, 47);
     function timer(){
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 317){ //if time value is greater than 549
+        if(time > 549){ //if time value is greater than 549
             clearInterval(counterLine); //clear counterLine
         }
     }
