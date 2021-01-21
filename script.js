15caba23b8bec5ea3a5a906a3197f3325111929a
rbse <77785147+rbse@users.noreply.github.com>
2021-01-22 00:16:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
-const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
@@ -27,7 +26,6 @@ continue_btn.onclick = ()=>{
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
     startTimer(25); //calling startTimer function
-    startTimerLine(0); //calling startTimerLine function
 }
 
 let timeValue =  25;
